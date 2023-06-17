1057736f5a86687d21f4f609aba549a56c3a27de
Michael P. Jung <michael.jung@terreon.de>
2023-06-17 14:58:02 +0200

    Improve parsing of CAN messages

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,50 @@
 use std::path::Path;
 
 use futures_util::stream::StreamExt;
-use socketcan::{Error, tokio::CanSocket, EmbeddedFrame};
+use model::State;
+use socketcan::{tokio::CanSocket, EmbeddedFrame, Frame};
 
-mod rotex;
+pub mod model;
+pub mod rotex;
 
+use model::Parameter;
 use rotex::RotexData;
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let rotex_data = RotexData::read(Path::new("rotex-data.json"))?;
 
-    let rotex_data = RotexData::read(Path::new("rotex-data.json"));
-    println!("{:?}", rotex_data);
+    let state = State::from_rotex_data(&rotex_data);
+    println!("{:?}", state);
 
     let mut socket_rx = CanSocket::open("can0")?;
     //let socket_tx = CanSocket::open("vcan0")?;
     while let Some(Ok(frame)) = socket_rx.next().await {
         if let socketcan::CanFrame::Data(data_frame) = frame {
-            println!("{:?}", data_frame.data());
+            match data_frame.data() {
+                [a0, a1, a2, p0, p1, v0, v1] => {
+                    let a = model::Address(frame.id_word(), *a0, *a1, *a2);
+                    let Some((d_name, op)) = state.device_by_address.get(&a) else {
+                        println!("Unsupported address: {:?}", a);
+                        continue;
+                    };
+                    let p = ((*p0 as u16) << 8) + (*p1 as u16);
+                    let v = ((*v0 as u16) << 8) + (*v1 as u16);
+                    let Some(p_name) = state.parameter_by_address.get(&p) else {
+                        println!("Unknown parameter: {}", p);
+                        continue;
+                    };
+                    let param = &state.parameters[p_name];
+                    match param {
+                        Parameter::Float { factor, .. } => {
+                            println!("{:?} {}.{} = {}", op, d_name, p_name, v as f32 / factor);
+                        }
+                    }
+                }
+                x => {
+                    println!("{} {:?}", frame.id_word(), x);
+                }
+            }
         }
         //socket_tx.write_frame(frame)?.await?;
     }
--- src/rotex.rs
+++ src/rotex.rs
@@ -1,5 +1,7 @@
 use std::fmt;
 use std::io::BufReader;
+use std::marker::PhantomData;
+use std::num::ParseIntError;
 use std::path::Path;
 use std::{fs::File, io};
 
@@ -7,29 +9,62 @@ use serde::de::Unexpected;
 use serde::{Deserialize, Deserializer};
 use thiserror::Error;
 
-fn deserialize_u8_hex<'de, D>(deserializer: D) -> Result<u8, D::Error>
+pub trait FromHexStr
 where
-    D: Deserializer<'de>,
+    Self: Sized,
 {
-    struct HexStringVisitor;
-    impl<'de> serde::de::Visitor<'de> for HexStringVisitor {
-        type Value = u8;
+    fn from_hex_str(s: &str) -> Result<Self, ParseIntError>;
+}
 
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str(
-                "a string containing a u8 encoded as hexadecimal string prefixed with '0x'",
-            )
-        }
-
-        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(0)
-            //serde_json::from_str(v).map_err(E::custom)
-        }
+impl FromHexStr for u8 {
+    fn from_hex_str(s: &str) -> Result<u8, ParseIntError> {
+        u8::from_str_radix(s, 16)
     }
-    deserializer.deserialize_str(HexStringVisitor)
+}
+
+impl FromHexStr for u32 {
+    fn from_hex_str(s: &str) -> Result<u32, ParseIntError> {
+        u32::from_str_radix(s, 16)
+    }
+}
+
+#[derive(Debug)]
+pub struct HexStr<T: FromHexStr>(pub T);
+
+impl<'de, T: FromHexStr> Deserialize<'de> for HexStr<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct HexStringVisitor<T: FromHexStr>(PhantomData<T>);
+
+        impl<'de, T: FromHexStr> serde::de::Visitor<'de> for HexStringVisitor<T> {
+            type Value = T;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str(
+                    "a string containing an integer encoded as hexadecimal string prefixed with '0x'",
+                )
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                if v.len() < 3 {
+                    return Err(E::invalid_value(Unexpected::Str(v), &self));
+                }
+                if !v.starts_with("0x") {
+                    return Err(E::invalid_value(Unexpected::Str(v), &self));
+                }
+                T::from_hex_str(&v[2..]).map_err(|_| E::invalid_value(Unexpected::Str(v), &self))
+            }
+        }
+
+        let visitor = HexStringVisitor(PhantomData::<T>::default());
+
+        deserializer.deserialize_str(visitor).map(HexStr)
+    }
 }
 
 fn deserialize_f32_str<'de, D>(deserializer: D) -> Result<f32, D::Error>
@@ -55,17 +90,19 @@ where
     deserializer.deserialize_str(FloatStringVisitor)
 }
 
-fn deserialize_option_bool_str<'de, D>(deserializer: D) -> Result<Option<bool>, D::Error>
+#[derive(Debug, Deserialize)]
+pub struct U16Str(#[serde(deserialize_with = "deserialize_u16_str")] pub u16);
+
+fn deserialize_u16_str<'de, D>(deserializer: D) -> Result<u16, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let s: Option<&str> = Option::deserialize(deserializer)?;
-    if let Some(s) = s {
-        Ok(Some(s.parse::<bool>().map_err(serde::de::Error::custom)?))
-    } else {
-        Ok(None)
-    }
+    let s = String::deserialize(deserializer)?;
+    s.parse::<u16>().map_err(serde::de::Error::custom)
 }
+
+#[derive(Debug, Deserialize)]
+pub struct F32Str(#[serde(deserialize_with = "deserialize_f32_str")] pub f32);
 
 fn deserialize_bool_str<'de, D>(deserializer: D) -> Result<bool, D::Error>
 where
@@ -73,6 +110,18 @@ where
 {
     let s = String::deserialize(deserializer)?;
     s.parse::<bool>().map_err(serde::de::Error::custom)
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BoolStr(#[serde(deserialize_with = "deserialize_bool_str")] pub bool);
+
+impl BoolStr {
+    fn true_value() -> Self {
+        Self(true)
+    }
+    fn false_value() -> Self {
+        Self(false)
+    }
 }
 
 #[derive(Error, Debug)]
@@ -84,8 +133,12 @@ pub enum LoadError {
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct RotexData {
     pub parameters: Vec<Parameter>,
+    pub heat_generators: Vec<Device>,
+    pub heating_circuits: Vec<Device>,
+    pub heating_circuit_modules: Vec<Device>,
 }
 
 impl RotexData {
@@ -97,72 +150,80 @@ impl RotexData {
     }
 }
 
-fn bool_true() -> bool {
-    true
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Device {
+    pub name: String,
+    pub get: (HexStr<u32>, HexStr<u8>, HexStr<u8>, HexStr<u8>),
+    pub set: (HexStr<u32>, HexStr<u8>, HexStr<u8>, HexStr<u8>),
+    pub answer: (HexStr<u32>, HexStr<u8>, HexStr<u8>, HexStr<u8>),
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Parameter {
     pub name: String,
-    #[serde(alias = "infoNumber")]
     pub info_number: InfoNumber,
     #[serde(flatten)]
     pub r#type: ParameterType,
-    #[serde(default, deserialize_with = "deserialize_bool_str")]
-    pub writeable: bool,
-    #[serde(default = "bool_true", deserialize_with = "deserialize_bool_str")]
-    pub display: bool,
-    #[serde(alias = "waterCircuit", default, deserialize_with = "deserialize_bool_str")]
-    pub water_circuit: bool,
+    #[serde(default = "BoolStr::false_value")]
+    pub writeable: BoolStr,
+    #[serde(default = "BoolStr::true_value")]
+    pub display: BoolStr,
+    #[serde(default = "BoolStr::false_value")]
+    pub water_circuit: BoolStr,
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct InfoNumber {
-    #[serde(alias = "byteHigh", deserialize_with = "deserialize_u8_hex")]
-    pub byte_high: u8,
-    #[serde(alias = "byteLow", deserialize_with = "deserialize_u8_hex")]
-    pub byte_low: u8,
+    pub byte_high: HexStr<u8>,
+    pub byte_low: HexStr<u8>,
+}
+
+impl InfoNumber {
+    pub fn as_u16(&self) -> u16 {
+        ((self.byte_high.0 as u16) << 8) + (self.byte_low.0 as u16)
+    }
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(tag = "type")]
+#[serde(rename_all = "camelCase")]
 pub enum ParameterType {
     #[serde(alias = "bool")]
     Bool {
-        #[serde(deserialize_with = "deserialize_option_bool_str")]
-        default: Option<bool>,
+        default: Option<BoolStr>,
     },
     #[serde(alias = "int")]
     Int {
-        #[serde(alias = "bigEndian", default, deserialize_with = "deserialize_bool_str")]
-        big_endian: bool,
-        min: Option<u16>,
-        max: Option<u16>,
-        default: Option<u16>,
+        #[serde(default = "BoolStr::false_value")]
+        big_endian: BoolStr,
+        min: Option<U16Str>,
+        max: Option<U16Str>,
+        default: Option<U16Str>,
         #[serde(alias = "transferDelay")]
-        transfer_delay: Option<u16>,
+        transfer_delay: Option<U16Str>,
         #[serde(alias = "delayedByValue")]
-        delayed_by_value: Option<Vec<u16>>,
+        delayed_by_value: Option<Vec<U16Str>>,
     },
     #[serde(alias = "float")]
     Float {
-        #[serde(alias = "bigEndian", default, deserialize_with = "deserialize_bool_str")]
-        big_endian: bool,
-        #[serde(alias = "transferThreshold", deserialize_with = "deserialize_f32_str")]
-        transfer_threshold: f32,
-        #[serde(alias = "transferThreshold", deserialize_with = "deserialize_f32_str")]
-        factor: f32,
-        min: Option<f32>,
-        max: Option<f32>,
-        default: Option<f32>,
+        #[serde(default = "BoolStr::false_value")]
+        big_endian: BoolStr,
+        transfer_threshold: Option<F32Str>,
+        factor: F32Str,
+        min: Option<F32Str>,
+        max: Option<F32Str>,
+        default: Option<F32Str>,
     },
     #[serde(alias = "enum")]
     Enum {
-        #[serde(alias = "bigEndian", default, deserialize_with = "deserialize_bool_str")]
-        big_endian: bool,
-        r#in: Option<Vec<u8>>,
+        #[serde(default = "BoolStr::false_value")]
+        big_endian: BoolStr,
+        r#in: Option<Vec<U16Str>>,
     },
     TimeRange {
-        default: String,
+        default: Option<String>,
     },
 }
