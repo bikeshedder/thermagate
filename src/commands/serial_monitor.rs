6a689c65a11ef414e725b628e7fa64639a52ef22
Michael P. Jung <michael.jung@terreon.de>
2023-08-26 21:31:57 +0200

    Move opening of serial stream into its own module

--- src/commands/serial_monitor.rs
+++ src/commands/serial_monitor.rs
@@ -1,19 +1,13 @@
 use tokio::io::AsyncReadExt;
-use tokio_serial::SerialPortBuilderExt;
 
-use crate::config::Config;
+use crate::{config::Config, serial};
 
 pub async fn cmd(config: Config) -> Result<(), Box<dyn std::error::Error>> {
-    let mut receiver = tokio_serial::new(&config.serial.path, config.serial.baud_rate)
-        .data_bits(config.serial.data_bits)
-        .parity(config.serial.parity)
-        .stop_bits(config.serial.stop_bits)
-        .flow_control(config.serial.flow_control)
-        .open_native_async()?;
+    let mut stream = serial::open_stream(&config.serial)?;
     println!("Receiving from {} ...", config.serial.path);
     let mut buf = [0u8; 32];
     loop {
-        let count = receiver.read(&mut buf).await?;
+        let count = stream.read(&mut buf).await?;
         if count == 0 {
             continue;
         }
--- src/serial.rs
+++ src/serial.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize};
+use tokio_serial::{SerialPortBuilderExt, SerialStream, Error};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SerialConfig {
@@ -18,4 +19,12 @@ pub fn reg_query(reg_id: u8) -> Vec<u8> {
     let mut msg = vec![0x02, reg_id];
     msg.push(crc(&msg));
     msg
+}
+
+pub fn open_stream(config: &SerialConfig) -> Result<SerialStream, Error> {
+    tokio_serial::new(&config.path, config.baud_rate)
+        .data_bits(tokio_serial::DataBits::Eight)
+        .parity(tokio_serial::Parity::Even)
+        .stop_bits(tokio_serial::StopBits::One)
+        .open_native_async()
 }
