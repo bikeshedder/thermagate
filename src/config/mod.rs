a18bb4d6a3f6852a689f998839aea30ed9daec8c
Michael P. Jung <michael.jung@terreon.de>
2023-08-26 21:27:52 +0200

    Convert config module into a directory

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::serial::SerialConfig;
 
-pub const DEFAULT_CONFIG: &str = include_str!("config.default.toml");
+pub const DEFAULT_CONFIG: &str = include_str!("default.toml");
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
