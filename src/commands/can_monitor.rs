52362887653a6785588ac84ff80b611448192ed2
Michael P. Jung <michael.jung@terreon.de>
2023-12-22 23:12:20 +0100

    Add new CanDriver

--- src/commands/can_monitor.rs
+++ src/commands/can_monitor.rs
@@ -1,14 +1,16 @@
 use console::style;
 use itertools::Itertools;
-use socketcan::{CanFrame, CanSocket, EmbeddedFrame, Frame, Socket};
+use socketcan::EmbeddedFrame;
 
 use crate::can::device::Device;
-use crate::can::message::{Message, MessageType};
+use crate::can::driver::CanDriver;
+use crate::can::message::MessageType;
 use crate::can::params::PARAMS;
 use crate::config::Config;
 
 pub async fn cmd(config: Config) -> Result<(), Box<dyn std::error::Error>> {
-    let socket = CanSocket::open(&config.can.interface).unwrap();
+    let driver = CanDriver::new(&config.can.interface);
+    let mut rx = driver.rx();
 
     let req = style("REQ").cyan();
     let rsp = style("RSP").green();
@@ -16,78 +18,68 @@ pub async fn cmd(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     let ping = style("PING").yellow();
     let pong = style("PONG").yellow();
 
-    while let Ok(frame) = socket.read_frame() {
-        let CanFrame::Data(data_frame) = frame else {
-            continue;
-        };
-        let data = data_frame.data();
-        let msg = Message::try_from(&data_frame);
-        if msg
-            .as_ref()
-            .is_ok_and(|m| m.sender == Device::G1 || m.receiver == Device::G1)
-        {
+    loop {
+        let msg = rx.recv().await?;
+        if msg.sender == Device::G1 || msg.receiver == Device::G1 {
             // filter messages from Rocon G1
             continue;
         }
-        print!("{:03x} ", frame.raw_id());
-        let data = data.iter().map(|b| format!("{:02x}", b)).join(" ");
+        print!("{:03x} ", u16::from(msg.sender));
+        let data = msg
+            .frame
+            .data()
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .join(" ");
         print!("{} ", style(data).dim());
-        match msg {
-            Ok(msg) => {
-                let sender = style(format!(" {:7} ", msg.sender.to_string()))
-                    .on_color256(233)
-                    .white();
-                let receiver = style(format!(" {:7} ", msg.receiver.to_string()))
-                    .on_color256(233)
-                    .white();
-                let param = msg.param;
-                match msg.r#type {
-                    MessageType::Set => {
-                        print!("{sender} -> {receiver} {set} {param:04x} ")
-                    }
-                    MessageType::Request => {
-                        print!("{sender} -> {receiver} {req} {param:04x} ")
-                    }
-                    MessageType::Response => {
-                        print!("{receiver} <- {sender} {rsp} {param:04x} ")
-                    }
-                    MessageType::Ping => {
-                        print!("{sender} -> {receiver} {ping} {param:04x} ")
-                    }
-                    MessageType::Pong => {
-                        print!("{receiver} <- {sender} {pong} {param:04x} ")
-                    }
-                    MessageType::Unknown(x) => {
-                        print!("{sender} -> {receiver} UNKNOWN({x})")
-                    }
-                }
-                if msg.r#type == MessageType::Request
-                    || msg.r#type == MessageType::Response
-                    || msg.r#type == MessageType::Set
-                {
-                    if let Some(p) = PARAMS.get(&msg.param) {
-                        print!("{} ", style(&p.name()).magenta());
-                        if msg.r#type == MessageType::Response || msg.r#type == MessageType::Set {
-                            let decoded_data = p.decode_any(msg.data);
-                            if let Some(decoded_data) = decoded_data {
-                                print!("= {} ", style(decoded_data).bold());
-                            } else {
-                                print!("= {} ", style("None").italic());
-                            }
-                        }
-                    } else if msg.r#type == MessageType::Response || msg.r#type == MessageType::Set
-                    {
-                        print!(
-                            "= {}",
-                            msg.data.iter().map(|b| format!("{:02x}", b)).join("")
-                        );
-                    }
-                }
-                println!();
+        let sender = style(format!(" {:7} ", msg.sender.to_string()))
+            .on_color256(233)
+            .white();
+        let receiver = style(format!(" {:7} ", msg.receiver.to_string()))
+            .on_color256(233)
+            .white();
+        let param = msg.param;
+        match msg.r#type {
+            MessageType::Set => {
+                print!("{sender} -> {receiver} {set} {param:04x} ")
             }
-            Err(e) => println!("Error: {}", e),
+            MessageType::Request => {
+                print!("{sender} -> {receiver} {req} {param:04x} ")
+            }
+            MessageType::Response => {
+                print!("{receiver} <- {sender} {rsp} {param:04x} ")
+            }
+            MessageType::Ping => {
+                print!("{sender} -> {receiver} {ping} {param:04x} ")
+            }
+            MessageType::Pong => {
+                print!("{receiver} <- {sender} {pong} {param:04x} ")
+            }
+            MessageType::Unknown(x) => {
+                print!("{sender} -> {receiver} UNKNOWN({x})")
+            }
         }
+        if msg.r#type == MessageType::Request
+            || msg.r#type == MessageType::Response
+            || msg.r#type == MessageType::Set
+        {
+            if let Some(p) = PARAMS.get(&msg.param) {
+                print!("{} ", style(&p.name()).magenta());
+                if msg.r#type == MessageType::Response || msg.r#type == MessageType::Set {
+                    let decoded_data = p.decode_any(msg.data);
+                    if let Some(decoded_data) = decoded_data {
+                        print!("= {} ", style(decoded_data).bold());
+                    } else {
+                        print!("= {} ", style("None").italic());
+                    }
+                }
+            } else if msg.r#type == MessageType::Response || msg.r#type == MessageType::Set {
+                print!(
+                    "= {}",
+                    msg.data.iter().map(|b| format!("{:02x}", b)).join("")
+                );
+            }
+        }
+        println!();
     }
-
-    Ok(())
 }
